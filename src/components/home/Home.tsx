2c46ef122dbc9451f9fecf0e5d8d2bf95de3ab6e
Revati Tambe <24revati@gmail.com>
2026-01-05 19:39:34 -0800

    npx fix-react2shell-next

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../../../@/components/ui/tabs"
 import CSS from "csstype";
 
-import './Home.css'
 import { Link } from "react-router-dom";
 
 const Home = () => {
